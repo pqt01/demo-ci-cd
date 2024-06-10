3aa9073fc64c0e5a2f7fed2461deca8d1d0a3543
pqt01 <thinhpqse151077@fpt.edu.vn>
2024-06-10 18:07:02 +0700

    create v2

--- Demo-ci-cd/Controllers/WeatherForecastController.cs
+++ Demo-ci-cd/Controllers/WeatherForecastController.cs
@@ -29,5 +29,16 @@ namespace Demo_ci_cd.Controllers
             })
             .ToArray();
         }
+        [HttpGet("V2")]
+        public IEnumerable<WeatherForecast> GetV2()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
